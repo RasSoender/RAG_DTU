d99d793557959ace6a4845a7437d24915a897053
soender2 <s214925@dtu.dk>
2025-03-22 12:49:02 +0100

    Scraping course is working, but needs to be implemented for all courses

--- scraping_course.py
+++ scraping_course.py
@@ -6,47 +6,124 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 import time
+import json
 
-chrome_options = Options()
-chrome_options.add_argument("--headless")  # Run in the background (remove if you want to see the browser)
-chrome_options.add_argument("--disable-blink-features=AutomationControlled")
+def setup_driver(headless=True):
+    """Set up and return a Selenium WebDriver."""
+    chrome_options = Options()
+    if headless:
+        chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-blink-features=AutomationControlled")
 
-# Start the WebDriver
-service = Service(ChromeDriverManager().install())
-driver = webdriver.Chrome(service=service, options=chrome_options)
+    service = Service(ChromeDriverManager().install())
+    return webdriver.Chrome(service=service, options=chrome_options)
 
-# Open the webpage
-url = "https://kurser.dtu.dk/course/47301"  # Update to the correct URL
-driver.get(url)
 
-time.sleep(2)  # Adjust as needed
+def click_button(driver, wait_time=10):
+    try:
+        button = WebDriverWait(driver, wait_time).until(EC.element_to_be_clickable((By.XPATH, "//div[@class='pull-right']//span//a")))
+        button.click()
+        time.sleep(2)  # Adjust as needed
+    except Exception as e:
+        print(f"Error clicking button:", e)
 
-button = driver.find_element(By.XPATH, "//div[@class='pull-right']//span//a")
-button.click()
 
-time.sleep(2)
-driver.refresh() 
+def scrape_text(driver, xpath, wait_time=10):
+    """Retrieve text from an element given its XPath."""
+    try:
+        element = WebDriverWait(driver, wait_time).until(EC.presence_of_element_located((By.XPATH, xpath)))
+        return element.text.strip()
+    except Exception as e:
+        return None
 
-# Wait for the <h2> element to load
-try:
 
-    h2_element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.XPATH, "//h2"))
-    )
+def saving_into_json(scraped_info):
+    course_no = scraped_info["course title"][:5]
+    with open(f"data_courses/{course_no}.json", "w", encoding="utf-8") as f:
+        json.dump(scraped_info, f, indent=4) # dump() will overwrite
+        print("JSON file saved successfully!")
+    return None
 
-    course_title = h2_element.text
-    print("Course Title:", course_title)
+def scraping_elements_left(driver, j, scraped_info):
+    i = 1
+    while True: 
+        key = scrape_text(driver, f"//div[@class='box information']//table[{j}]//tr[{i}]//td[1]")
+        if not key:
+            break
+        value = scrape_text(driver, f"//div[@class='box information']//table[{j}]//tr[{i}]//td[2]")
+        scraped_info[key] = value
+        i += 1
+    return scraped_info
 
-    label_element = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.XPATH, "//div[@class='box information']//table//tr//td/label"))
-    )
-    
-    # Get the text of the label element
-    label_text = label_element.text
-    print("Label Text:", label_text)  
+def scraping_elements_right(driver, scraped_info):
 
-except Exception as e:
-    print("Error:", e)
+    def split_text_by_titles(text, titles, scraped_info):
+        lines = text.split("\n")  # Split text into lines
+        
+        title_n = len(titles)
+        title_index = 1
+        current_title = titles[0]
+        next_title = titles[1]
+        current_content = []
 
-# Close the driver
-driver.quit()
+        for line in lines:
+            line = line.strip()
+            if line == next_title:
+                scraped_info[current_title] = "\n".join(current_content).strip()
+                
+                current_title = next_title
+                current_content = []
+                title_index += 1
+                if title_index < title_n:                  
+                    next_title = titles[title_index]
+            else:
+                current_content.append(line)
+
+        # Save the last section
+        if current_title:
+            scraped_info[current_title] = "\n".join(current_content).strip()
+
+        return scraped_info 
+
+    i = 1
+    all_keys = []
+    while True:
+        key = scrape_text(driver, f"//div[@class='col-md-6 col-sm-12 col-xs-12']//div[@class='box']//div[@class='bar'][{i}]")
+        if not key:
+            break
+        all_keys.append(key)
+        i += 1
+
+    all_text = scrape_text(driver, "//div[@class='col-md-6 col-sm-12 col-xs-12']//div[@class='box']")
+    scraped_info = split_text_by_titles(all_text, all_keys, scraped_info)
+    return scraped_info
+
+
+def main():
+    url = "https://kurser.dtu.dk/course/47301"
+
+    driver = setup_driver(headless=True)  # Set to False if you want to see the browser
+    driver.get(url)
+    time.sleep(2)
+
+    # Click the button to get english version of page
+    click_button(driver)
+
+    # Refresh page
+    driver.refresh()
+    time.sleep(2)
+
+    scraped_info = dict()
+    scraped_info["course title"] = scrape_text(driver, "//h2")
+
+    for j in range(1, 4):
+        scraped_info = scraping_elements_left(driver, j, scraped_info)
+
+    scraped_info = scraping_elements_right(driver, scraped_info)
+
+    saving_into_json(scraped_info)
+    driver.quit()
+
+
+if __name__ == "__main__":
+    main()
