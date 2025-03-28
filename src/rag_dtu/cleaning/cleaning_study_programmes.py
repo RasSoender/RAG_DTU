c5a68d5787f0de084fa8bae37d9ca9ca9a8af329
soender2 <s214925@dtu.dk>
2025-03-28 22:09:17 +0100

    cleaning the data of study programmes

---
+++ src/rag_dtu/cleaning/cleaning_study_programmes.py
@@ -0,0 +1,106 @@
+import os
+import json
+
+titles = [
+    "Official title",
+    "About the Programme Specification",
+    "Duration",
+    "General admission requirements",
+    "Academic requirements for this programme",
+    "Competence profile",
+    "Programme specific goals for learning outcome",
+    "Structure",
+    "Programme provision",
+    "Specializations",
+    "Curriculum",
+    "Curriculum, previous admission years",
+    "Master's thesis",
+    "Master thesis, specific rules",
+    "Study Activity Requirements and Deadlines",
+    "Study Programme Rules",
+    "Regarding course descriptions",
+    "Course registration",
+    "Binding courses",
+    "Academic prerequisites for course participation",
+    "Participation in limited admission courses",
+    "Mandatory participation in class and exam prerequisites",
+    "Deadlines for publication of teaching material and syllabus",
+    "Project courses",
+    "Evaluation of teaching",
+    "Exam rules",
+    "Credit Transfer, Studying Abroad, Exemption, Leave, etc.",
+    "Head of study"
+]   
+
+
+def split_text_by_titles(text):
+    scraped_info = {}
+
+    lines = text.split("\n")  # Split text into lines
+    
+    title_n = len(titles)
+
+    title_index = 1
+    current_title = titles[0]
+    next_title = titles[1] if title_index < title_n else None
+    current_content = []
+
+    for line in lines:
+        line = line.strip()
+
+        if line.startswith("## "):
+            line = line[3:].strip()  # Remove "## " and strip the rest
+            if line == "Official title":
+                current_content = []
+
+        if next_title and line == next_title:
+            scraped_info[current_title] = "\n".join(current_content).strip()
+            
+            current_title = next_title
+            current_content = []
+            title_index += 1
+            next_title = titles[title_index] if title_index < title_n else None
+        else:
+            current_content.append(line)
+
+    # Save the last section
+    if current_title:
+        if current_title == "Head of study":
+            scraped_info[current_title] = "\n".join(current_content[:5]).strip()
+        else: 
+            scraped_info[current_title] = "\n".join(current_content).strip()
+
+    return scraped_info
+
+
+def clean_data(text):
+    result = split_text_by_titles(text)
+    return result
+
+
+
+
+# Folder containing all the course JSON files
+folder_path = 'data/data_study_programmes'
+
+# Loop through each file in the folder
+for filename in os.listdir(folder_path):
+    if filename.endswith('.json'):
+
+        # Construct full file path
+        file_path = os.path.join(folder_path, filename)
+
+        # Read the JSON content from the file
+        with open(file_path, 'r', encoding='utf-8') as file:
+            course_data = json.load(file)
+
+        programme_info = clean_data(course_data["markdown"])
+
+        # Save the combined data into a new JSON file
+        output_file = f'data/data_study_programmes_cleaned/{filename}'
+
+        with open(output_file, 'w', encoding='utf-8') as output:
+             json.dump(programme_info, output, ensure_ascii=False, indent=4)
+    
+
+print(f"The markdown files of the study programmes has been saved into {output_file}")
